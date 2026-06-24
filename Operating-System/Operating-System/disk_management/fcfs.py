f27fe36fa0f9a639227fad89382d34bbd9108006
Gerald <geraldtrogado@gmail.com>
2026-06-24 19:40:08 +0800

    moved virtual memory to a new folder location and deleted unnecessary duplicate folder.

--- Operating-System/Operating-System/disk_management/fcfs.py
+++
@@ -1,7 +0,0 @@
-from utils import calculate_movement
-
-class FCFSScheduler:
-    def execute(self, head, requests):
-        """First-Come, First-Served"""
-        sequence = [head] + requests.copy()
-        return {"sequence": sequence, "total_movement": calculate_movement(sequence)}
